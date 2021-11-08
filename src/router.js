fdbfdea8a927936a316a632f9183e28f289d827e
Leandro Cotti <52415035+Lefcott@users.noreply.github.com>
2021-11-08 02:05:38 -0300

    [TOR-4] Instalación de react router (#1)

--- src/router.js
+++ src/router.js
@@ -1,9 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Layout from "./components/Layout";
+import RouletteScreen from "./screens/roulette";
+import HomeScreen from "./screens/home";
 
 export default function Router() {
   return (
-    <div className="App">
-      <Layout>layout</Layout>
-    </div>
+    <Layout>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="roulette" element={<RouletteScreen />} />
+        </Routes>
+      </BrowserRouter>
+    </Layout>
   );
 }
---
+++ src/screens/roulette/index.js
@@ -0,0 +1,3 @@
+export default function RouletteScreen() {
+  return <div>Roulette Screen</div>;
+}
