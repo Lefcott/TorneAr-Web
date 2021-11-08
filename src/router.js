453fe1eeffff058a70371aead435d9127ce2007c
Leandro Cotti <lefcott@hotmail.com>
2021-11-08 01:42:50 -0300

    TOR-4 configured router and added basic screen

--- src/router.js
+++ src/router.js
@@ -2,11 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "./components/Layout";
 import RouletteScreen from "./screens/roulette";
+import HomeScreen from "./screens/home";
 
 export default function Router() {
   return (
     <Layout>
-      <RouletteScreen />
+      <BrowserRouter>
+        <Routes>
+          <Route path="roulette" element={<RouletteScreen />} />
+          <Route path="/" element={<HomeScreen />} />
+        </Routes>
+      </BrowserRouter>
     </Layout>
   );
 }
