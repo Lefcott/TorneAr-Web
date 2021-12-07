77140f285598c37bb0ab72e85601bcfcb0619fdb
Leandro Cotti <lefcott@hotmail.com>
2021-12-07 17:21:34 -0300

    TOR-21 removed graphql dep

--- src/screens/roulette/index.js
+++ src/screens/roulette/index.js
@@ -1,35 +1,6 @@
-import { useEffect } from "react";
 import Roulette from "components/Roulette";
-import getRouletteSocket from "common/socket/roulette";
-import { useQuery } from "@apollo/client";
-import game from "queries/game";
 
 export default function RouletteScreen() {
-  const rouletteSocket = getRouletteSocket();
-  const { data } = useQuery(game, {
-    variables: { code: "roulette" },
-  });
-
-  if (data) {
-    console.log("got game data!", data);
-  }
-
-  useEffect(() => {
-    const handleBetStarted = (params) => {
-      console.log("bet has started!", params);
-    };
-    const handleBetEnded = (params) => {
-      console.log("bet has ended!", params);
-    };
-    rouletteSocket.on("betStarted", handleBetStarted);
-    rouletteSocket.on("betEnded", handleBetEnded);
-
-    return () => {
-      rouletteSocket.off("betStarted", handleBetStarted);
-      rouletteSocket.off("betEnded", handleBetEnded);
-    };
-  });
-
   return (
     <div>
       <Roulette />
