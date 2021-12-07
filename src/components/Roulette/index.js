343dbeeafbd0ae9f9496b7317ba89e95bfe2cdb5
Leandro Cotti <lefcott@hotmail.com>
2021-12-07 17:34:34 -0300

    TOR-21 little adjustments

--- src/components/Roulette/index.js
+++ src/components/Roulette/index.js
@@ -1,6 +1,6 @@
 import indicatorsrc from "./images/indicator.png";
 import style from "./style.module.scss";
-import ButtonComponent from "./ButtonList";
+import ButtonList from "./ButtonList";
 import Timer from "./Timer";
 import Wheel from "./Wheel";
 
@@ -12,7 +12,7 @@ const Roulette = () => {
         <img className={style.indicator} src={indicatorsrc} />
         <Timer />
       </div>
-      <ButtonComponent />
+      <ButtonList />
     </>
   );
 };
--- src/queries/user.js
+++
@@ -1,1 +0,0 @@
-export const createUser = () => 3;
