0ac2615251237b7c7f8faaf5e6e4c1418ef5c3ba
Leandro Cotti <lefcott@hotmail.com>
2021-12-08 14:45:50 -0300

    TOR-44 replaced <a> tags with Link component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 import style from "./style.module.css";
 import ConnectedUsers from "./ConnectedUsers";
 
 export default function Navbar() {
   return (
     <nav className={style.navbar}>
-      <a className={style.link} href="/">
+      <Link className={style.link} to="/">
         <img src="/images/logo.png" className={style.logo} />
         <div className={style.navbar_title}>TorneAr</div>
-      </a>
+      </Link>
       <ConnectedUsers />
     </nav>
   );
